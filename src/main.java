e5f79a5e56d4dff8fd605874a48aa36827a5aa12
HashMap <e9047689558@yandex.ru>
2022-02-17 14:04:24 +0300

    betterTest

--- src/main.java
+++ src/main.java
@@ -1,7 +1,12 @@
 public class main
 {
+    //the best test of the world!
     public static void main(String[] args)
     {
-        System.out.println("Hello");
+        MyHashMap myy = new MyHashMap();
+        for (int i = 0; i < 2; i++)
+            myy.put(i," value: "+i);
+        for (int i = 0; i < myy.size(); i++)
+            System.out.println(myy.get(i));
     }
 }
