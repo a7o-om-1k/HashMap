f4bbc4abc739eefda260865c3a0b73e7047de1c4
HashMap <e9047689558@yandex.ru>
2022-02-17 13:54:40 +0300

    addAll

--- src/MyHashMap.java
+++ src/MyHashMap.java
@@ -43,12 +43,7 @@ public class MyHashMap<k,v> implements MyMapInterface
 
     @Override
     public boolean isEmty() {
-        return size ==0;
-    }
-
-    @Override
-    public Object get(Object key) {
-        return null;
+        return size == 0;
     }
 
     //get HashCode and place where store
@@ -64,7 +59,42 @@ public class MyHashMap<k,v> implements MyMapInterface
         return hashValue % length;
     }
 
+    //copy array
+    public void Transfer (Node [] src, Node [] newTable)
+    {
+        int newCapacity = newTable.length;
+        for (int j = 0; j <src.length; j ++) {
+            Node e = src [j];
+            if (e != null) {
+                src [j] = null; //
+                do {
+                    Node next = e.next;
+                    int i = indexFor (e.hash, newCapacity);
+                    e.next = newTable [i];
+                    newTable [i] = e;
+                    e = next;
+                } while (e != null);
+            }
+        }
+    }
 
+
+    //add element to Entry, where hash - value hash
+    public void addEntry(Object key, Object value, int hashValue, int i)
+    {
+        // if length arrray is more then previosly agreed, expanding capacity
+        if (++size >= table.length * DEFAULT_LOAD_FACTOR)
+        {
+            Node[] newTable = new Node[table.length << 1];
+            //copy array
+            Transfer(table, newTable);
+            table = newTable;
+        }
+        //get data to i
+        Node eNode = table[i];
+        //add Node, and indicate Node near to prevoius
+        table[i] = new Node(hashValue, key, value, eNode);
+    }
 
     @Override
     public Object put(Object key, Object value)
@@ -76,10 +106,29 @@ public class MyHashMap<k,v> implements MyMapInterface
         // if i have data and same key - > overwrite!
         for (Node node = table[i]; node != null; node = node.next)
         {
+            Object k;
+            if (node.hash == hashValue && ((k = node.key)==key||key.equals(k)))
+            {
+                Object oldValue = node.value;
+                node.value = value;
+                return oldValue;
+            }
+        }
+        addEntry(key, value, hashValue, i);
+        return null;
+    }
+
+    @Override
+    public Object get(Object key) {
+        //get value by hashCode
+        int hashValue = hash(key);
+        //by hash and size Node get index put position
+        int i = indexFor(hashValue, table.length);
+        for (Node node = table[i]; node != null; node = node.next)
+        {
             if (node.key.equals(key) && hashValue == node.hash) return node.value;
         }
         return null;
     }
-
 
 }
